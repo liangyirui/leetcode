cfc06068c0a82073681bc967fc8123a08d78be19
Yirui Liang <41648750+liangyirui@users.noreply.github.com>
2024-07-19 15:28:35 -0700

    Add Problem 1530

---
+++ problems/1530_leaf_nodes_pairs.py
@@ -0,0 +1,28 @@
+"""
+https://leetcode.com/problems/number-of-good-leaf-nodes-pairs
+
+1530. Number of Good Leaf Nodes Pairs
+
+You are given the root of a binary tree and an integer distance. A pair of two different leaf nodes of a binary tree is said to be good if the length of the shortest path between them is less than or equal to distance.
+Return the number of good leaf node pairs in the tree.
+"""
+
+from classes import TreeNode
+
+
+def count_pairs(root: TreeNode, distance: int) -> int:
+    count = 0
+
+    def dfs(node: TreeNode | None) -> list[int]:
+        nonlocal count
+        if node is None:
+            return []
+        if node.left is None and node.right is None:
+            return [1]
+        left = dfs(node.left)
+        right = dfs(node.right)
+        count += sum(l_dist + r_dist <= distance for l_dist in left for r_dist in right)
+        return [n + 1 for n in left + right if n <= distance]
+
+    dfs(root)
+    return count
